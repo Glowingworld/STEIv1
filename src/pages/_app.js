fe0da39c08563e05033d2c2bab04fa40e842ceb9
marenga14 <marengajulius@gmail.com>
2023-11-24 15:05:33 +0300

    seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,15 +4,29 @@ import { ProSidebarProvider } from "react-pro-sidebar";
 import { useState } from "react";
 import { MantineProvider, Button } from "@mantine/core";
 import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <ProSidebarProvider>
-        <Component {...pageProps} />
-      </ProSidebarProvider>
-    </SessionProvider>
+    <>
+      <DefaultSeo
+        title="Stei"
+        description="Stei is for renting house in Tanzania, nyumba za kupagisha Tanzania"
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://stei.co.tz",
+          siteName: "SiteName",
+        }}
+      />
+
+      <SessionProvider session={pageProps.session}>
+        <ProSidebarProvider>
+          <Component {...pageProps} />
+        </ProSidebarProvider>
+      </SessionProvider>
+    </>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="Stei" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
